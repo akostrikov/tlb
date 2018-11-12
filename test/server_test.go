52be54533c3035d4d6a168a01d7584d95ef8fd09
Andrey Smetanin <irqlevel@gmail.com>
2018-11-13 00:53:36 +0300

    true test parallelism

--- test/server_test.go
+++ test/server_test.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"net"
 	"net/http"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -14,19 +15,24 @@ func TestServer(t *testing.T) {
 	t.Parallel()
 	t.Logf("serverAddress %s", *serverAddress)
 
-	client := &http.Client{
-		Timeout: time.Second * 10,
-		Transport: &http.Transport{
-			Dial: (&net.Dialer{
-				Timeout: 5 * time.Second,
-			}).Dial,
-			TLSHandshakeTimeout: 5 * time.Second,
-			DisableKeepAlives:   true,
-		}}
+	for i := 0; i < 10000; i++ {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+			client := &http.Client{
+				Timeout: time.Second * 10,
+				Transport: &http.Transport{
+					Dial: (&net.Dialer{
+						Timeout: 5 * time.Second,
+					}).Dial,
+					TLSHandshakeTimeout: 5 * time.Second,
+					DisableKeepAlives:   true,
+				}}
 
-	resp, err := client.Get("http://" + *serverAddress + "/blank")
-	if err != nil {
-		t.Fatalf("Get failed with err %v", err)
+			resp, err := client.Get("http://" + *serverAddress + "/blank")
+			if err != nil {
+				t.Fatalf("Get failed with err %v", err)
+			}
+			defer resp.Body.Close()
+		})
 	}
-	defer resp.Body.Close()
 }
